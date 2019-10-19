a12c53810a0d341002b12900f4c0602af4c7955e
sjkchang <sjkchang@gmail.com>
2019-10-18 21:24:22 -0700

    Added CSS elements, and created simple about.html, and register.html templates

--- Cloze/cloze.py
+++ Cloze/cloze.py
@@ -8,12 +8,21 @@ app.debug = False
 #app.config['SECRET_KEY'] = 'some-key'
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html', title = 'login')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'about')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title = 'register')
 
 if __name__ == '__main__':
     app.debug = True
