b2e7b1f81044679ecfcc678dd6f9c2f76ee71913
sjkchang <steven.chang@sjsu.edu>
2019-12-02 10:20:32 -0800

    Heroku test 3

--- cloze/__init__.py
+++ cloze/__init__.py
@@ -8,7 +8,7 @@ login_manager = LoginManager()
 bcrypt = Bcrypt()
 
 def create_app(testing):
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__)
 
     if testing == False:
         # load the instance config, if it exists, when not testing
